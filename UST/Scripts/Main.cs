1ae8a6095544e83822759d6ab8b7f09ca8c6c0c8
Kevin Vu <kevin.m.vu@gmail.com>
2025-10-19 03:17:14 -0700

    Add more prototype data

--- UST/Scripts/Main.cs
+++ UST/Scripts/Main.cs
@@ -1,4 +1,5 @@
 using Godot;
+using System.Collections.ObjectModel;
 
 namespace UST.Scripts;
 
@@ -10,4 +11,105 @@ public partial class Main : Node {
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {
     }
+}
+
+public enum UnitType {
+    Rogue,
+    Trader,
+    Knight,
+    Mage,
+    Blacksmith
+}
+
+public enum BuildingType {
+    Tavern,
+    Arcanum,
+    Barracks,
+    Blacksmith,
+    Castle,
+    House,
+    Market
+}
+
+public enum ResourceType {
+    Gold,
+    Stone,
+    Brick,
+    Water,
+    Wood,
+    Food,
+    Oil,
+    Minerals
+}
+
+public enum LocationType {
+    Swamp,
+    Mountains,
+    Plateau,
+    Desert,
+    Temperate,
+    Wasteland,
+    Tropics
+}
+
+public enum PathType {
+    GravelPavement,
+    DirtPavement,
+    BrickPavement,
+    StoneSlabPavement,
+    AsphaltPavement,
+    CobblestonePavement,
+    Swamp,
+    Mountain,
+    Sand,
+    Water,
+    Forest,
+    Ice,
+    Snow,
+    Plains,
+    Mesa
+}
+
+public class Unit {
+    public string Name { get; set; }
+    public UnitType Type { get; set; }
+    public float Health { get; set; }
+    public float Defense { get; set; }
+    public float AttackDamage { get; set; }
+    public float AttackSpeed { get; set; }
+    public float Accuracy { get; set; }
+    public float Evasion { get; set; }
+    public float MovementSpeed { get; set; }
+}
+
+public class Building {
+    public string Name { get; set; }
+    public BuildingType Type { get; set; }
+    public float Health { get; set; }
+    public float Defense { get; set; }
+    public float AttackDamage { get; set; }
+    public float AttackSpeed { get; set; }
+    public float Accuracy { get; set; }
+}
+
+public class Location {
+    public string Name { get; set; }
+    public LocationType Type { get; set; }
+    public Collection<Unit> Units { get; }
+    public Collection<Building> Buildings { get; }
+    public Collection<Resource> Resources { get; }
+}
+
+public class Resource {
+    public string Name { get; set; }
+    public ResourceType Type { get; set; }
+    public float Amount { get; set; }
+}
+
+public class Path {
+    public string Name { get; set; }
+    public PathType Type { get; set; }
+    public Location Location1 { get; set; }
+    public Location Location2 { get; set; }
+    public float Distance { get; set; }
 }
