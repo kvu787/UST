1bc92a1a8df2c27d78e5a42f5ecae40a786986c5
Kevin Vu <kevin.m.vu@gmail.com>
2025-11-19 12:42:40 -0800

    Upgrade to .NET 10 LTS and C# 14

--- UST/Scripts/Main.cs
+++ UST/Scripts/Main.cs
@@ -1,4 +1,6 @@
 using Godot;
+using System;
+using System.Collections.Generic;
 
 namespace UST;
 
@@ -15,13 +17,42 @@ public partial class Main : Node {
         GD.Print($"Game data folder: {ProjectSettings.GlobalizePath("user://")}");
 
         MainInstance = this;
-        this.Terminal = new();
+        this.Terminal = new Terminal();
 
         this.TerminalLineEdit = this.GetNode<LineEdit>("LineEdit");
         this.TerminalLineEdit.GuiInput += this.OnTerminalSubmit;
 
         this.TerminalLineEdit.FocusEntered += () => this.TerminalLineEdit.PlaceholderText = "";
         this.TerminalLineEdit.FocusExited += () => this.TerminalLineEdit.PlaceholderText = "Click here. Type a command. Press ENTER to submit.";
+    }
+
+    private static Dictionary<int, HashSet<int>> GenerateMap(int numLocations, (int, int) numConnectionsRange) {
+        Dictionary<int, HashSet<int>> map = [];
+        Random random = new();
+
+        for (int i = 0; i < numLocations; i++) {
+            map.Add(i, []);
+
+            // Create a pool of valid candidate nodes (all nodes except self)
+            List<int> candidates = [];
+            for (int j = 0; j < numLocations; j++) {
+                if (j != i) {
+                    candidates.Add(j);
+                }
+            }
+
+            // Randomly decide how many connections to add (1 to 4)
+            int connectionCount = random.Next(numConnectionsRange.Item1, numConnectionsRange.Item2 + 1);
+
+            // Randomly pick nodes from the candidates
+            for (int k = 0; k < connectionCount; k++) {
+                int randomIndex = random.Next(candidates.Count);
+                _ = map[i].Add(candidates[randomIndex]);
+                candidates.RemoveAt(randomIndex);
+            }
+        }
+
+        return map;
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
