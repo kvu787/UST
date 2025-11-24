d49ce72c8e8e413c23441e2de557026c456dbfc7
Kevin Vu <kevin.m.vu@gmail.com>
2025-11-24 07:18:15 -0800

    Add logic for location capturing and victory check

--- UST/Code/Main.cs
+++ UST/Code/Main.cs
@@ -9,22 +9,11 @@ public partial class Main : Node {
         GD.Print($"Game data folder: {ProjectSettings.GlobalizePath("user://")}");
 
         Globals.Terminal = new Terminal(this, this.GetNode<LineEdit>("LineEdit"));
-
-        /*
-        Generate the graph
-        Generate mapping of graph vertices to team ownership (1 or 2)
-        Generate units
-        Place units on map
-        */
         Globals.SaveData = new SaveData(numLocations: 10, probabilityAddExtraEdge: 0.2, proportionTeamOne: 0.65);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {
-        /*
-        Loop through and execute each unit's automaton
-        if win condition is met, exit loop and print results
-        */
         Globals.SaveData.Process(delta);
     }
 }
--- UST/Code/SaveData.cs
+++ UST/Code/SaveData.cs
@@ -1,4 +1,6 @@
+using Godot;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace UST;
 
@@ -21,6 +23,10 @@ public class SaveData {
     private List<int> Teams;
 
     private List<UnitRow> UnitRows;
+
+    /// <summary>
+    /// Maps each location to the set of units in that location.
+    /// </summary>
     public Dictionary<int, HashSet<UnitRow>> UnitLocations { get; }
 
     public SaveData(int numLocations, double probabilityAddExtraEdge, double proportionTeamOne) {
@@ -29,5 +35,19 @@ public class SaveData {
     }
 
     public void Process(double delta) {
+        // Capture locations that contain units from one team
+        foreach ((int location, HashSet<UnitRow> units) in this.UnitLocations) {
+            if (units.GroupBy(x => x.Team).Count() == 1) {
+                this.Teams[location] = units.First().Team;
+            }
+        }
+
+        // Check if all locations captured by one team
+        if (this.Teams.GroupBy(x => x).Count() == 1) {
+            GD.Print($"Team {this.Teams[0]} Victory!");
+        }
+
+        // Move units
+        // Attack units
     }
 }
