762f90d1f64d5ad42e53e0f61fcf5967e359fb2e
Kevin Vu <kevin.m.vu@gmail.com>
2026-02-15 22:06:21 -0800

    Add PrintSystemInfo

--- UST/Code/Main.cs
+++ UST/Code/Main.cs
@@ -1,8 +1,21 @@
 using Godot;
 using System;
+using System.Runtime.InteropServices;
 using System.Text;
+using Environment = System.Environment;
 
 namespace UST;
+
+/// <summary>
+/// Olivia: Walks around, invulnerable
+/// Liam: Walks around, dies after set amount of time
+/// Emma: Walks around with variable speed, dies after variable amount of time
+/// Noah: Seeks out food to eat to increase life time
+/// Amelia: Can fish from lakes
+/// Oliver: Requires shelter
+/// Charlotte: Requires water
+/// 
+/// </summary>
 
 public partial class Main : Node {
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2213:Disposable fields should be disposed", Justification = "This object should last the entire process lifecycle")]
@@ -16,8 +29,11 @@ public partial class Main : Node {
 
         GD.Print("Ready");
         GD.Print($"Game data folder: {ProjectSettings.GlobalizePath("user://")}");
+        GD.Print();
+
         this.TextOutput = this.GetNode<TextEdit>("TextEdit");
 
+        PrintSystemInfo();
 
         Globals.Terminal = new Terminal(this, this.GetNode<LineEdit>("LineEdit"));
         Globals.World = new World();
@@ -31,6 +47,32 @@ public partial class Main : Node {
         foreach (Unit unit in Globals.World.Units) {
             GD.Print($"Unit {unit.Id,2}: {unit.Health,6:###.00}, {unit.MoveSpeed:F2}, {unit.Attack:F2}, {unit.Location}, {unit.Team}");
         }
+    }
+
+    private static void PrintSystemInfo() {
+        GD.Print("=== Runtime Info ===");
+        GD.Print($"RuntimeInformation.FrameworkDescription: {RuntimeInformation.FrameworkDescription}");
+        GD.Print($"RuntimeInformation.OSDescription: {RuntimeInformation.OSDescription}");
+        GD.Print($"RuntimeInformation.ProcessArchitecture: {RuntimeInformation.ProcessArchitecture}");
+        GD.Print($"Environment.Version: {Environment.Version}");
+        GD.Print($"AppContext.TargetFrameworkName: {AppContext.TargetFrameworkName}");
+        GD.Print();
+
+        GD.Print("=== Process Info ===");
+        GD.Print($"Environment.ProcessId: {Environment.ProcessId}");
+        GD.Print($"Environment.ProcessPath: {Environment.ProcessPath}");
+        GD.Print($"Environment.CurrentDirectory: {Environment.CurrentDirectory}");
+        GD.Print();
+
+        GD.Print("=== Machine Info ===");
+        GD.Print($"Environment.MachineName: {Environment.MachineName}");
+        GD.Print($"Environment.ProcessorCount: {Environment.ProcessorCount}");
+        GD.Print($"Environment.Is64BitOperatingSystem: {Environment.Is64BitOperatingSystem}");
+        GD.Print();
+
+        GD.Print("=== Memory ===");
+        GD.Print($"GC.GetTotalMemory: {GC.GetTotalMemory(false)}");
+        GD.Print();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
